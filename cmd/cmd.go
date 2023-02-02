45cc197d3396d24c52ca77e0b2a2da07586c740e
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-02-02 15:42:14 +0000

    made cmd.go more readable

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,64 +3,63 @@ package cmd
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/mbaraa/eloi/utils"
 )
 
+var (
+	flags        = flag.NewFlagSet("Eloi", flag.ExitOnError)
+	meow         = new(bool)
+	download     = new(bool)
+	syncRepoName = new(string)
+	ebuildName   = new(string)
+	repoName     = new(string)
+)
+
 func Start() {
-	var meow bool
-	flag.BoolVar(&meow, "meow", false, "meow meow meow")
+	registerFlags()
+	flags.Parse(os.Args[1:])
+	runWithGivenArgs()
+}
 
-	var download bool
-	flag.BoolVar(&download, "download", false, "download overlays repos cache")
-
-	var ebuildName string
-	flag.StringVar(&ebuildName, "S", "", "search for an ebuild")
-	flag.StringVar(&ebuildName, "search", "", "search for an ebuild")
-
-	var overlayName string
-	flag.StringVar(&overlayName, "enable", "", "add a specific repository")
-
-	var sync bool
-	flag.BoolVar(&sync, "sync", false, "sync portage repos")
-
-	flag.Parse()
-
-	////
-
-	if meow {
+func runWithGivenArgs() {
+	if *meow {
 		fmt.Println("meow meow meow")
-	}
-
-	if len(overlayName) != 0 {
-		utils.AssertRoot()
-		err := utils.AddOverlayRepo(overlayName)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	if sync {
-		utils.AssertRoot()
-		err := utils.Sync()
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	if download {
+	} else if *download {
 		utils.AssertRoot()
 		err := utils.DownloadReposCache()
 		if err != nil {
 			panic(err)
 		}
-	}
-
-	if ebuildName != "" {
+	} else if len(*ebuildName) > 0 {
 		err := utils.LoadLocalOverlays()
 		if err != nil {
 			panic("Local repos are not synced, run with --sync to sync them")
 		}
-		utils.FindEbuild(ebuildName)
+		utils.FindEbuild(*ebuildName)
+	} else if len(*repoName) > 0 {
+		utils.AssertRoot()
+		err := utils.AddOverlayRepo(*repoName)
+		if err != nil {
+			panic(err)
+		}
+	} else if len(*syncRepoName) > 0 {
+		utils.AssertRoot()
+		err := utils.Sync(*syncRepoName)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		flags.Usage()
 	}
+}
+
+func registerFlags() {
+	flags.BoolVar(meow, "meow", false, "meow meow meow")
+	flags.BoolVar(download, "download", false, "download overlays repos cache")
+	flags.StringVar(ebuildName, "search", "", "find ebuild to install")
+	flags.StringVar(ebuildName, "S", "", "find ebuild to install")
+	flags.StringVar(repoName, "enable", "", "add a specific repository")
+	flags.StringVar(syncRepoName, "sync", "", "sync portage repos")
 }
