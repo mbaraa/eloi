91c72f4943fb8544c33bc2c68c0464544815f1c9
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-03-05 21:57:23 +0000

    updated ebuild's cli template

--- cli/templates/ebuild.go
+++ cli/templates/ebuild.go
@@ -1,52 +1,53 @@
 package templates
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/mbaraa/eloi/cli/cfmt"
+
 	"github.com/mbaraa/eloi/models"
 )
 
-func EbuildTemplate(ebuildWithVersions map[string]models.Ebuild) string {
+func EbuildTemplate(ebuild models.Ebuild) string {
 	/*
-
 	   * group-ebuild
-	       Available versions: 1.2 1.3 1.4
-	       Homepage:  https://link.com
-	       Description:   something really descriptive
-	       Available Overlays:    overlay1, overlay2, ...
-
-	       or
-
-	       Available versions:    overlay1(1.2 1.3) overlay2(1.4 **9999)
+	       Homepage:    	   https://link.com
+	       Description:   	   something really descriptive
+	       Available overlays: overlay1, overlay2
+	       Available versions: 1.2, 1.3, 1.4, **9999
 	*/
 
 	sb := new(strings.Builder)
 
-	providers := make(map[string][]string) // overlayName => version
-	var name, groupName, license string
-	for _, ebuild := range ebuildWithVersions {
-		name, groupName, license = ebuild.Name, ebuild.GroupName, ebuild.License
-		providers[ebuild.OverlayName] = append(providers[ebuild.OverlayName], ebuild.Version)
+	_, _ = cfmt.Green().Fprint(sb, ebuild.GroupName)
+	_, _ = cfmt.White().Fprint(sb, "/")
+	_, _ = cfmt.White().Bold().Fprintln(sb, ebuild.Name)
+
+	if len(ebuild.Homepage) != 0 {
+		_, _ = cfmt.Green().Bold().Fprintf(sb, "\tHomepage: ")
+		_, _ = cfmt.White().Fprintln(sb, ebuild.Homepage)
 	}
 
-	versionsStr := new(strings.Builder) // overlayN(versionN...)...
-	for name, versions := range providers {
-		cfmt.Yellow().Fprint(versionsStr, name+"(")
-		for i, version := range versions {
-			cfmt.Yellow().Fprint(versionsStr, version)
-			if i < len(versions)-1 {
-				versionsStr.WriteString(" ")
-			}
+	// if len(ebuild.Description) != 0 {
+	// 	_, _ = cfmt.Green().Bold().Fprintf(sb, "\tDescription: ")
+	// 	_, _ = cfmt.White().Fprintln(sb, ebuild.Description)
+	// }
+
+	versions := new(strings.Builder)
+	overlays := new(strings.Builder)
+	for i, extraData := range ebuild.ExtraData {
+		_, _ = cfmt.Yellow().Fprint(versions, extraData.Version)
+		_, _ = cfmt.Yellow().Fprint(overlays, extraData.OverlayName)
+		if i < len(ebuild.ExtraData)-1 {
+			_, _ = cfmt.Yellow().Fprint(versions, ", ")
+			_, _ = cfmt.Yellow().Fprint(overlays, ", ")
 		}
-		cfmt.Yellow().Fprint(versionsStr, ") ")
 	}
 
-	fmt.Fprintf(sb, "%s/%s\n", cfmt.Green().Sprint(groupName), cfmt.Bold().Sprint(name))
-	fmt.Fprintf(sb, "\t%s %s\n", cfmt.Green().Sprint("Available versions:"), versionsStr.String())
-	if len(license) != 0 {
-		fmt.Fprintf(sb, "\t%s %s\n", cfmt.Green().Sprint("License:"), license)
-	}
+	_, _ = cfmt.Green().Bold().Fprintf(sb, "\tAvailable Overlays: ")
+	_, _ = cfmt.Yellow().Fprintln(sb, overlays.String())
+	_, _ = cfmt.Green().Bold().Fprintf(sb, "\tAvailable Versions: ")
+	_, _ = cfmt.Yellow().Fprintln(sb, versions.String())
+
 	return sb.String()
 }
